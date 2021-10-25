f6a3e10921d38d2df3d45d95742201d70bef7a4a
Malte Schmitz <malte@schmitz-sh.de>
2021-10-25 22:36:35 +0200

    Add lines to video

--- art.js
+++ art.js
@@ -1,5 +1,5 @@
 (() => {
-  const canvasWidth = 20;
+  const canvasWidth = 100;
   let streaming = false;
 
   function newSvg(tag) {
@@ -10,6 +10,7 @@
   let canvas = document.createElement('canvas');
   let svg = document.getElementById('svg');
 
+  // attach webcam to video
   navigator.mediaDevices.getUserMedia({video: true, audio: false})
   .then(stream => {
     video.srcObject = stream;
@@ -24,10 +25,24 @@
       canvas.height = canvasWidth / ratio;
       svg.setAttribute("viewBox", "0 0 " + canvas.width + " " + canvas.height);
       streaming = true;
+      createLines();
       update();
-      setInterval(() => update(), 1000);
+      setInterval(() => update(), 100);
     }
   };
+
+  function createLines() {
+    svg.innerHTML = "";
+    for (let y = 0; y < canvas.height; y++) {
+      let path = newSvg("path");
+      path.setAttribute("stroke", "black");
+      path.setAttribute("stroke-width", "0.1");
+      path.setAttribute("fill", "transparent");
+      // path.setAttribute("d", `M 1,${y} L ${canvas.width},${y}`);
+      svg.appendChild(path); 
+    }
+    console.log(svg.getElementsByTagName('path'));
+  }
   
   function update() {
     let context = canvas.getContext('2d');
@@ -42,17 +57,46 @@
       }
     }
 
-    svg.innerHTML = "";
-    for (let x = 0; x < canvas.width; x++) {
-      for (let y = 0; y < canvas.height; y++) {
-        let circle = newSvg("circle");
-        circle.setAttribute("r", 0.4);
-        circle.setAttribute("cx", x + 0.5);
-        circle.setAttribute("cy", y + 0.5);
-        circle.setAttribute("fill", "rgb(" + map[x][y][0] + "," + map[x][y][1] + "," + map[x][y][2] + ")");
-        svg.appendChild(circle);
+    let paths = svg.getElementsByTagName('path');
+    for (let y = 0; y < canvas.height; y++) {
+      let points = [];
+      for (let x = 0; x < canvas.width; x++) {
+        let [red, green, blue, alpha] = map[x][y];
+        let brightness = 1 - (0.2126 * red / 255 + 0.7152 * green / 255 + 0.0722 * blue / 255);
+        points.push([0.5 + x, 0.5 + y + 2.5 - 5*brightness]);
       }
+      points = [[0, points[0][1]], [0, points[0][1]], ...points, points[points.length-1], [canvas.width, points[points.length-1][1]], [canvas.width, points[points.length-1][1]]];
+      let cmPoints = catmullRom(points);
+      paths[y].setAttribute("d", bezierre(points[1], cmPoints));
     }
+  }
+
+  function bezierre(start, points) {
+    let d = `M ${start[0]} ${start[1]} `;
+    for (let i = 0; i < points.length; i++) {
+      let p = points[i];
+      d += `C ${p[0][0]} ${p[0][1]}, ${p[1][0]} ${p[1][1]}, ${p[2][0]} ${p[2][1]} `;
+    }
+    return d;
+  }
+
+  function catmullRom(points) {
+    let m = [];
+    for (let i = 1; i < points.length - 1; i++) {
+      m[i] = [
+        (points[i][0]-points[i-1][0])/2 + (points[i+1][0]-points[i][0])/2,
+        (points[i][1]-points[i-1][1])/2 + (points[i+1][1]-points[i][1])/2
+        ];
+    }
+    let result = [];
+    for (let i = 1; i < points.length - 2; i++) {
+      result.push([
+        [points[i][0] + m[i][0]/3, points[i][1] + m[i][1]/3],
+        [points[i+1][0] - m[i+1][0]/3, points[i+1][1] - m[i+1][1]/3],
+        points[i+1]
+      ]);
+    }
+    return result;
   }
 
   function toggleFullScreen() {
